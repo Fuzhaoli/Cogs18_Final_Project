97c76a60aa90bcc3dc956faeb239e53fe8319cb9
Fuzhaoli <55896940+Fuzhaoli@users.noreply.github.com>
2019-12-11 00:06:17 -0800

    Delete test_functions.py

--- test_functions.py
+++
@@ -1,9 +0,0 @@
-import random
-from my_module.functions import *
-
-def test_chatbot():
-    assert end('Quit') == True
-    assert selector(['A','B','C'],['A','B','C','D','E'],['R']) == 'R'
-    assert buy_cd('Can I buy CD?') == 'We sell CD at the livehouse'
-    assert fan_meeting('Is there a fan meeting?') == 'No there is not any fan meeting'
-    assert other_artist('What about other artists?') == 'Sorry we only sell tickets of Ryo'
